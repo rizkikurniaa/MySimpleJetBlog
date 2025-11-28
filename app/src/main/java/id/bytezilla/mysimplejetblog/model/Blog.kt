1d4cf29149b4f3b93e90db4b45d4361c63a84971
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-28 22:31:53 +0700

    Remove description property from Blog data class

--- app/src/main/java/id/bytezilla/mysimplejetblog/model/Blog.kt
+++ app/src/main/java/id/bytezilla/mysimplejetblog/model/Blog.kt
@@ -3,7 +3,6 @@ package id.bytezilla.mysimplejetblog.model
 data class Blog(
     val id: Long,
     val title: String,
-    val description: String,
     val fullContent: String,
     val imageUrl: String,
     val author: String,
