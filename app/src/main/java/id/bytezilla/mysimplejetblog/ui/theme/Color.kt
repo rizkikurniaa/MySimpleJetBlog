2910b321c94167e7975d36fad8e086decdd51039
rizki <rizkikurniawan1797@gmail.com>
2025-12-01 21:43:59 +0700

    Update color palette from purple to green variants

--- app/src/main/java/id/bytezilla/mysimplejetblog/ui/theme/Color.kt
+++ app/src/main/java/id/bytezilla/mysimplejetblog/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package id.bytezilla.mysimplejetblog.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PastelGreen = Color(0xFFA5D6A7)
+val LightPastelGreen = Color(0xFFC8E6C9)
+val ExtraLightPastelGreen = Color(0xFFE8F5E9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkGreen = Color(0xFF388E3C)
+val DarkerGreen = Color(0xFF1B5E20)
+val AnotherDarkGreen = Color(0xFF2E7D32)
